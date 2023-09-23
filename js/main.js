7ffae217cc479e9c3b203e6661009405d0a4b5a0
Mauricio Gonzalez <58447379+evilmauri@users.noreply.github.com>
2023-09-23 12:39:59 -0300

    update

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ botonesCategorias.forEach(boton => {
         const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
         tituloPrincipal.innerText= productoCategoria.categoria.nombre;
         const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-
         cargarProductos(productosBoton);
         }else{
             tituloPrincipal.innerText = "Todos los productos";
